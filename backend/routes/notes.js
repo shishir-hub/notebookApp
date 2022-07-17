720f434e5930b6915a0d443e221d92d24c727ae6
Shishir <shishir.gaire11@gmail.com>
2022-07-17 15:21:53 +0545

    Updated with Update,Delete

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -45,4 +45,55 @@ router.post('/addnote', fetchuser, [
 
 });
 
+//Route3: Update existing Note using: PUT "/api/notes/updatenote". requires login
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+
+    try {
+
+        // Create a newNote Object
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+
+        //Find the note to be updated and update it
+        let notes = await Notes.findById(req.params.id);
+        if (!notes) { res.status(404).send("Not Found") };
+
+        if (notes.user.toString() !== req.user.id) {
+            if (!notes) { res.status(401).send("Not Allowed") };
+        }
+
+        notes = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json(notes);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error.");
+    }
+
+});
+
+//Route4: Delete existing Note using: DELETE "/api/notes/deletenote". requires login
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+    try {
+
+        //Find the note to be deleted and delete it
+        let notes = await Notes.findById(req.params.id);
+        if (!notes) { res.status(404).send("Not Found") };
+
+        //Allow deletion only if user owns the Note
+        if (notes.user.toString() !== req.user.id) {
+            if (!notes) { res.status(401).send("Not Allowed") };
+        }
+
+        notes = await Notes.findByIdAndDelete(req.params.id);
+        res.json({ "Success": "Note has been deleted", notes: notes });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error.");
+    }
+});
+
 module.exports = router;
